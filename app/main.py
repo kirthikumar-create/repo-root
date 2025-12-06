79165c40216713558fcfe199c17a848c2da5a3d1
kirthikumar-create <kirthi.kumar@enhub.ai>
2025-12-06 17:50:11 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 import asyncio
 from .scheduler import start_scheduler
+from .database import init_db
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "FastAPI  App Running on ECS Fargate"}
+    return {"message": "FastAPI App Running on ECS Fargate"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
 
 @app.on_event("startup")
 async def startup_event():
+    init_db()
     asyncio.create_task(start_scheduler())
